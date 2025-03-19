8a456f5aba317e4c60a50e1ef9bbb90a57508d45
Boualili Youcef <youcefboualili0@gmail.com>
2025-03-19 10:01:49 +0100

    mise a jour

--- exo02/check_all_dirs.py
+++ exo02/check_all_dirs.py
@@ -6,17 +6,17 @@ def get_user_dirs():
     with open("/etc/passwd") as f:
         for line in f:
             parts = line.split(":")
-            if parts[3] == "1000":  # Group ID 1000
-                user_dirs.append(parts[5])  # Home directory
+            if parts[3] == "1000":  
+                user_dirs.append(parts[5])
     return user_dirs
 
-def check_all_user_dirs(threshold):
+def check_all_user_dirs(seuil):
     user_dirs = get_user_dirs()
     problematic_users = []
 
     for user_dir in user_dirs:
-        result = subprocess.run(['python3', 'check_dir.py', user_dir, str(threshold)], capture_output=True)
-        if result.returncode != 1:
+        result = subprocess.run(['python3', 'check_dir.py', user_dir, str(seuil)], capture_output=True)
+        if result.returncode == 255:
             problematic_users.append(user_dir)
     
     if problematic_users:
@@ -28,10 +28,10 @@ def check_all_user_dirs(threshold):
 
 if __name__ == "__main__":
     try:
-        threshold = int(input("Entrez le seuil de fichiers : "))
-        if threshold < 0:
+        seuil = int(input("Entrez le seuil de fichiers : "))
+        if seuil < 0:
             print("Erreur : le seuil doit être un nombre entier positif.")
         else:
-            check_all_user_dirs(threshold)
+            check_all_user_dirs(seuil)
     except ValueError:
         print("Erreur : veuillez entrer un nombre entier valide pour le seuil.")
--- exo02/check_dir.py
+++ exo02/check_dir.py
@@ -1,25 +1,27 @@
 import os
 import sys
 
-def check_files(directory, threshold):
+def check_files(directory, seuil):
     file_count = 0
     for dirpath, dirnames, filenames in os.walk(directory):
         file_count += len(filenames)
-        if file_count > threshold:
+        if file_count > seuil:
             return -1
     return 0
 
 if __name__ == '__main__':
     if len(sys.argv) != 3:
-        print("Usage: python3 check_dir.py <directory> <threshold>")
+        print("Usage: python3 check_dir.py <directory> <seuil>")
         sys.exit(1)
 
     directory = sys.argv[1]
-    threshold = int(sys.argv[2])
+    seuil = int(sys.argv[2])
 
-    result = check_files(directory, threshold)
+    result = check_files(directory, seuil)
 
     if result == -1:
-        print(f"Le répertoire {directory} contient plus de {threshold} fichiers.")
+        print(f"Le répertoire {directory} contient plus de {seuil} fichiers.")
+        sys.exit(-1)
     else:
-        print(f"Le répertoire {directory} ne dépasse pas {threshold} fichiers.")
+        print(f"Le répertoire {directory} ne dépasse pas {seuil} fichiers.")
+        sys.exit(0)
